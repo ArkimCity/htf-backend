ebe59fd76f580ff7e0c0e5f88907c09c6233b46e
ArkimCity <jaeung949@gmail.com>
2021-02-04 12:13:53 +0900

    init

---
+++ src/main/java/htf/backend/HtfBackendApplication.java
@@ -0,0 +1,13 @@
+package htf.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtfBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htf/backend/HtfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package htf.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
