bbf70cda7845f5bc6ac4030158d058b11deabb96
MG <alsrjs2441@naver.com>
2021-02-06 15:50:56 +0900

    Update LoginController.java

--- src/main/java/htf/backend/controller/LoginController.java
+++ src/main/java/htf/backend/controller/LoginController.java
@@ -1,23 +1,14 @@
 package htf.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import htf.backend.dao.MemberRepository;
 import htf.backend.domain.Member;
 import htf.backend.service.JWTService;
 import htf.backend.service.MemberService;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
