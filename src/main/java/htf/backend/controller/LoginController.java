3a6055d2049751fd6a96995695273475903b7a5f
seongho726 <sungho121@naver.com>
2021-02-07 13:07:35 +0900

    Update LoginController.java

--- src/main/java/htf/backend/controller/LoginController.java
+++ src/main/java/htf/backend/controller/LoginController.java
@@ -1,31 +1,19 @@
 package htf.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import htf.backend.dao.MemberRepository;
 import htf.backend.domain.Member;
 import htf.backend.service.JWTService;
 import htf.backend.service.MemberService;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @CrossOrigin
 @RestController
 public class LoginController {
 
-    @Autowired
-    private MemberRepository memberRepository;
     @Autowired
     private MemberService memberService;
     @Autowired
