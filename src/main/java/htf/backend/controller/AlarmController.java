ff5e235259320908a2a792fb347d4ebac6848ed2
seongho726 <sungho121@naver.com>
2021-02-14 21:56:35 +0900

    remove non used import

--- src/main/java/htf/backend/controller/AlarmController.java
+++ src/main/java/htf/backend/controller/AlarmController.java
@@ -7,13 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import htf.backend.domain.Admin;
-import htf.backend.domain.Member;
-import htf.backend.domain.Vendor;
 import htf.backend.kakao.KakaoService;
 import htf.backend.service.AdminService;
-import htf.backend.service.JWTService;
-import htf.backend.service.MemberService;
-import htf.backend.service.VendorService;
 import net.minidev.json.JSONObject;
 
 @CrossOrigin
